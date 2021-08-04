b1a4ab473db2396c3579feead1fed45afff6d299
IlfyPlorious <sdragosflorian@gmail.com>
2021-08-04 20:10:47 +0300

    interfata + link in java

--- Aplicatie Android/app/src/main/java/com/example/dummy/MainMenu.java
+++ Aplicatie Android/app/src/main/java/com/example/dummy/MainMenu.java
@@ -3,12 +3,24 @@ package com.example.dummy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button weighButton, registerButton;
+    private TextView kilos, connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        weighButton = findViewById(R.id.weighButton);
+        registerButton = findViewById(R.id.registerButton);
+        kilos = findViewById(R.id.kilos);
+        connection = findViewById(R.id.connection);
+        
+
     }
 }
