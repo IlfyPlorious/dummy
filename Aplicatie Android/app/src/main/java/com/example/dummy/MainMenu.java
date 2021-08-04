ffe909b9573457e94e5935d263921ba395faede3
DragosIonescu1509 <mrtataie00@gmail.com>
2021-08-04 21:20:55 +0300

    a doua incercare upload

--- Aplicatie Android/app/src/main/java/com/example/dummy/MainMenu.java
+++ Aplicatie Android/app/src/main/java/com/example/dummy/MainMenu.java
@@ -3,13 +3,20 @@ package com.example.dummy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.UUID;
 
 public class MainMenu extends AppCompatActivity {
 
     private Button weighButton, registerButton;
     private TextView kilos, connection;
+    private FirebaseFirestore firebaseFirestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,33 @@ public class MainMenu extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         kilos = findViewById(R.id.kilos);
         connection = findViewById(R.id.connection);
-        
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        weighButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weigh();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String weigh = UUID.randomUUID().toString();
+        String dbKilos;
+        dbKilos = kilos.getText().toString();
+        firebaseFirestore.collection("Weighs").document(weigh).set(dbKilos);
 
     }
+
+    private void weigh() {
+    }
+
+
 }
